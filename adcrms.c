afb60f83d5a1880a100fdaf2d04c70a5740650a8
Shreesh-Kulkarni <kshreesh5@gmail.com>
2023-03-29 16:08:31 +0530

    rms code for assignment.

---
+++ adcrms.c
@@ -0,0 +1,137 @@
+#include "DSP28x_Project.h"     // Device Headerfile and Examples Include File
+
+#include "f2802x_common/include/adc.h"
+#include "f2802x_common/include/clk.h"
+#include "f2802x_common/include/flash.h"
+#include "f2802x_common/include/gpio.h"
+#include "f2802x_common/include/pie.h"
+#include "f2802x_common/include/pll.h"
+#include "f2802x_common/include/wdog.h"
+#define VSF 3.3/4096
+
+// Prototype statements for functions found within this file.
+interrupt void adc_isr(void);
+void Adc_Config(void);
+
+// Global variables used in this example:
+uint16_t LoopCount;
+float adc_in;
+uint16_t ConversionCount;
+uint16_t adcresult[16];
+float32 adcVoltage[16];
+float32 rmsvalue;
+
+ADC_Handle myAdc;
+CLK_Handle myClk;
+FLASH_Handle myFlash;
+GPIO_Handle myGpio;
+PIE_Handle myPie;
+PWM_Handle myPwm;
+
+void main(void)
+{
+
+    CPU_Handle myCpu;
+    PLL_Handle myPll;
+    WDOG_Handle myWDog;
+
+    // Initialize all the handles needed for this application
+    myAdc = ADC_init((void *)ADC_BASE_ADDR, sizeof(ADC_Obj));
+    myClk = CLK_init((void *)CLK_BASE_ADDR, sizeof(CLK_Obj));
+    myCpu = CPU_init((void *)NULL, sizeof(CPU_Obj));
+    myFlash = FLASH_init((void *)FLASH_BASE_ADDR, sizeof(FLASH_Obj));
+    myGpio = GPIO_init((void *)GPIO_BASE_ADDR, sizeof(GPIO_Obj));
+    myPie = PIE_init((void *)PIE_BASE_ADDR, sizeof(PIE_Obj));
+    myPll = PLL_init((void *)PLL_BASE_ADDR, sizeof(PLL_Obj));
+    myPwm = PWM_init((void *)PWM_ePWM1_BASE_ADDR, sizeof(PWM_Obj));
+    myWDog = WDOG_init((void *)WDOG_BASE_ADDR, sizeof(WDOG_Obj));
+
+    // Perform basic system initialization
+    WDOG_disable(myWDog);
+    CLK_enableAdcClock(myClk);
+    (*Device_cal)();
+
+    //Select the internal oscillator 1 as the clock source
+    CLK_setOscSrc(myClk, CLK_OscSrc_Internal);
+
+    // Setup the PLL for x10 /2 which will yield 60Mhz = 10Mhz * 6 / 1
+    PLL_setup(myPll, PLL_Multiplier_6, PLL_DivideSelect_ClkIn_by_1);
+
+    // Disable the PIE and all interrupts
+    PIE_disable(myPie);
+    PIE_disableAllInts(myPie);
+    CPU_disableGlobalInts(myCpu);
+    CPU_clearIntFlags(myCpu);
+
+// If running from flash copy RAM only functions to RAM
+#ifdef _FLASH
+    memcpy(&RamfuncsRunStart, &RamfuncsLoadStart, (size_t)&RamfuncsLoadSize);
+#endif
+
+    // Setup a debug vector table and enable the PIE
+    PIE_setDebugIntVectorTable(myPie);
+    PIE_enable(myPie);
+
+    // Register interrupt handlers in the PIE vector table
+    PIE_registerPieIntHandler(myPie, PIE_GroupNumber_10, PIE_SubGroupNumber_1, (intVec_t)&adc_isr);
+
+    // Initialize the ADC
+    ADC_enableBandGap(myAdc);
+    ADC_enableRefBuffers(myAdc);
+    ADC_powerUp(myAdc);
+    ADC_enable(myAdc);
+    ADC_setVoltRefSrc(myAdc, ADC_VoltageRefSrc_Int);
+
+    // Enable ADCINT1 in PIE
+    PIE_enableAdcInt(myPie, ADC_IntNumber_1);
+    // Enable CPU Interrupt 1
+    CPU_enableInt(myCpu, CPU_IntNumber_10);
+    // Enable Global interrupt INTM
+    CPU_enableGlobalInts(myCpu);
+    // Enable Global realtime interrupt DBGM
+    CPU_enableDebugInt(myCpu);
+
+    LoopCount = 0;
+
+    // Configure ADC
+    //Note: Channel ADCINA4  will be double sampled to workaround the ADC 1st sample issue for rev0 silicon errata
+    ADC_setIntPulseGenMode(myAdc, ADC_IntPulseGenMode_Prior);               //ADCINT1 trips after AdcResults latch
+    ADC_enableInt(myAdc, ADC_IntNumber_1);                                  //Enabled ADCINT1
+    ADC_setIntMode(myAdc, ADC_IntNumber_1, ADC_IntMode_ClearFlag);          //Disable ADCINT1 Continuous mode
+    ADC_setIntSrc(myAdc, ADC_IntNumber_1, ADC_IntSrc_EOC0);                 //setup EOC0 to trigger ADCINT1 to fire
+    ADC_setSocChanNumber (myAdc, ADC_SocNumber_0, ADC_SocChanNumber_A0);    //set SOC0 channel select to ADCINA0
+    ADC_setSocTrigSrc(myAdc, ADC_SocNumber_0, ADC_SocTrigSrc_CpuTimer_0);    //set SOC0 start trigger on EPWM1A, due to round-robin SOC0 converts first then SOC1
+    ADC_setSocSampleWindow(myAdc, ADC_SocNumber_0, ADC_SocSampleWindow_14_cycles);   //set SOC0 S/H Window to 14 ADC Clock Cycles, (13 ACQPS plus 1)
+
+    // Wait for ADC interrupt
+    for(;;)
+    {
+        LoopCount++;
+    }
+
+}
+
+
+interrupt void adc_isr(void)
+{
+
+    //discard ADCRESULT0 as part of the workaround to the 1st sample errata for rev0
+    for(int i=0;i<16;i++){
+        adcresult[i]=ADC_readResult(myAdc,ADC_ResultNumber_0);
+        adcVoltage[i]]=VSF*adcresult[i];
+    }
+    float32 sum = 0;
+    for(uint16_t i=0; i<ADC_SOC_NUMBER; i++)
+    {
+        sum += adcVoltage[i] * adcVoltage[i];
+    }
+    rmsValue = sqrt(sum/16);
+    // Clear ADCINT1 flag reinitialize for next SOC
+    ADC_clearIntFlag(myAdc, ADC_IntNumber_1);
+    // Acknowledge interrupt to PIE
+    PIE_clearInt(myPie, PIE_GroupNumber_10);
+
+    return;
+}
+
+
